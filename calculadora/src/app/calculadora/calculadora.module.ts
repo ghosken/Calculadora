fb87ae7273bab970d196f25a5a122d9bce3caeb7
Gadu Hosken <gadu.gameiro@outlook.com>
2022-03-07 09:53:36 -0300

    Ajuste da kndex.ts

--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './component';
+import { CalculadoraService } from './services';
 
 @NgModule({
   declarations: [CalculadoraComponent],
   imports: [
     CommonModule
   ],
-  exports: [CalculadoraComponent] 
+  exports: [
+    CalculadoraComponent
+  ],
+  providers:[
+    CalculadoraService
+  ]
 })
 export class CalculadoraModule { }
